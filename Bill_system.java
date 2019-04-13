2a3fd14e281ee416cee61605f3b81e6514c36325
HIMANSHU KUMAR <49288924+himanshukumar199795@users.noreply.github.com>
2019-04-13 23:01:08 +0530

    java files

---
+++ Bill_system.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+package bill_system;
+
+
+public class Bill_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        bill_form Formbill=new bill_form();
+        Formbill.setVisible(true);
+        Formbill.setLocationRelativeTo(null);
+    }
+    
+}
